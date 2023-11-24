6c31ed118c7ee98da6711cf92c1865771e5e3ea2
Barbara Kosińska <basia7kosinska@gmail.com>
2023-11-24 12:21:22 +0100

    Add InitActions function

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,11 @@
 /* global Handlebars, utils, dataSource */ // eslint-disable-line no-unused-vars
 
+
 'use strict';
-
+//Prepare references to template & .book-lis
 const template = Handlebars.compile(document.querySelector('#template-book').innerHTML);
-console.log(template);
 const bookList = document.querySelector('.books-list');
-console.log(bookList);
-
+//Add render function
 function render (){
     for (let book in dataSource.books) {
         const generatedHTML = template(dataSource.books[book]);
@@ -14,5 +13,39 @@ function render (){
         bookList.appendChild(bookElement);
     }
 }
+
 render();
+
+
+let favouriteBooks = [];
+
+function initActions() {
+    const bookImages = bookList.querySelectorAll('.book__image');
+   
+    for (let image of bookImages) { 
+        const imageId = image.getAttribute('data-id');
+        const imageObject = {id:imageId, element: image};
+
+        image.addEventListener('dblclick', function(event){
+            event.preventDefault();
+
+            const isImageinFavourites = favouriteBooks.some(item => item.id == imageId);
+
+            if(isImageinFavourites){
+            image.classList.remove('favourite');
+            favouriteBooks = favouriteBooks.filter(item => item.id !== imageId);
+            console.log('po usunięcu :' + favouriteBooks);
+            } else {
+            image.classList.add('favourite');
+            favouriteBooks.push(imageObject);
+            console.log('po dodaniu ' + favouriteBooks);
+            }
+        });   
+        console.log('wynik tablicy: ' + favouriteBooks);
+    }
+}
+
+initActions();
+
+
 
